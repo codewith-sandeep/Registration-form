c9f9da372d7fb96e755a12da00d1df60bdf7dff1
sanju <104483128+codewith-sandeep@users.noreply.github.com>
2022-04-28 10:09:33 +0530

    --all

---
+++ footer.php
@@ -0,0 +1,74 @@
+<?php
+
+
+    <div class="card-footer">
+              <div class="row">
+                <div class="col"><h5>About</h5>
+                
+                   echo"<a href="" alt="about us">About us<br> </a>";
+                   echo<a href="">Careers<br> </a>
+                   <a href="">Contact us <br> </a> 
+                   <a href="">Corporate Information</a>  
+                 
+                  
+                </div>
+                <div class="col"><h5>Help</h5>
+               
+                    <a href="">Payment <br> </a>
+                    <a href="">Shipping  <br></a>
+                    <a href="">Cancellation & Return<br> </a> 
+                    <a href="">FAQ</a>   
+                 
+                </div>
+                <div class="col"><h5>Policy</h5>
+                  
+                    <a href="">Return Policy  <br></a>
+                    <a href="">Terms of use  <br></a>
+                    <a href="">Security  <br></a>
+                    <a href="">Privacy  <br></a>
+                    <a href="">Sitemap</a>  
+              
+                </div>
+                <div class="col"><h5>Social</h5>
+                 
+                    <i class="fa fa-facebook" aria-hidden="true"></i><a href="https://www.facebook.com/ ">Facebook <br> </a>
+                    <i class="fa fa-twitter" aria-hidden="true"></i><a href="https://www.teitter.com">Twitter  <br></a>
+                    <i class="fa fa-instagram" aria-hidden="true"></i><a href="https:/www.instagram.com">Instagram <br></a> 
+                    <i class="fa fa-linkedin" aria-hidden="true"></i><a href="https:/www.linkedin.com">LinkedIn  <br></a>
+                    <i class="fa fa-youtube-play" aria-hidden="true"></i><a href="https:/www.youtube.com">YouTube  <br></a>
+                  </ul>
+                </div>
+                <div class="col"><h5><i class="fa fa-envelope" aria-hidden="true"></i>Mail Us</h5>
+                  <a href="">writeus@looktobuy.com<br></a>
+                  <a href="">services@looktobuy.com<br></a>
+                </div>
+                <div class="col"><h6><i class="fa fa-address-book-o" aria-hidden="true"></i>Registered Office Address</h6>
+                  <p>892, Park Street, IT park Square, Mumbai(M.H.) PIN- 727290
+                  
+
+                </div>
+              </div>
+              <div class="row">
+              
+                <div class="col">
+                  <h6>Our offices:<br></h6>
+                  Australia<br> United States<br> India <br> Singapore<br> Japan <br> Germany<br> France<br>
+                  <div class="chc">LookToBuy<br>your choices are on the way</div>
+                </div>
+                <div class="col"><img  class="imgg" src="images\LookToBuy-logos.jpeg" alt="logo" height="200" width="400" ></div>
+                <div class="col">
+                  <div class="last">
+                   <i><h6>
+                      www.looktobuy.in<br>
+                    LookToBuy pvt. ltd.<br>
+                    Terms : Privacy :Regulations<br>
+                   2022 ©All Rights are reserved 
+                   </h6>
+                    
+                   </i> 
+                  </div>
+                  
+                </div>
+                                
+              </div>
+            </div>';?>
